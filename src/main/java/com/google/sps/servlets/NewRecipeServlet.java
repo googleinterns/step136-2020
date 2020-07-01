58ee61604011316934b7b0f50f435fddc82fa154
Isabel <isamitre@gmail.com>
2020-07-01 15:58:47 +0000

    added description

--- src/main/java/com/google/sps/servlets/NewRecipeServlet.java
+++ src/main/java/com/google/sps/servlets/NewRecipeServlet.java
@@ -1,20 +1,10 @@
 package com.google.sps.servlets;
 
-import com.google.appengine.api.blobstore.BlobInfo;
-import com.google.appengine.api.blobstore.BlobInfoFactory;
-import com.google.appengine.api.blobstore.BlobKey;
-import com.google.appengine.api.blobstore.BlobstoreService;
-import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.images.ImagesService;
-import com.google.appengine.api.images.ImagesServiceFactory;
-import com.google.appengine.api.images.ServingUrlOptions;
-import java.io.File;
+import com.google.appengine.api.datastore.Entity;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,9 +17,43 @@ public class NewRecipeServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    // implement new recipe
+    String recipeName = request.getParameter("recipeName");
+    String tagsResponse = request.getParameter("tags");
+    String description = request.getParameter("description");
+    String ingredientsResponse = request.getParameter("ingredients");
+    String stepsResponse = request.getParameter("steps");
+    String privacy = request.getParameter("privacy");
+    
+    // splits the String responses from the form by commas/newlines and makes the resulting arrays into Lists
+    List<String> tags = new ArrayList<String>(Arrays.asList(tagsResponse.split(",", 0)));
+    List<String> ingredients = new ArrayList<String>(Arrays.asList(ingredientsResponse.split("\n", 0)));
+    List<String> steps = new ArrayList<String>(Arrays.asList(stepsResponse.split("\n", 0)));
+
+    // removes any empty, null, or newline members of the Lists
+    tags.removeAll(Arrays.asList("", null));
+    ingredients.removeAll(Arrays.asList("", null, "\n", "\r\n", "\r"));
+    steps.removeAll(Arrays.asList("", null, "\n", "\r\n", "\r"));
+
+    for (String tag : tags) {
+      tag.trim();
+    }
+    for (String ingredient : ingredients) {
+      ingredient.trim();
+    }
+    for (String step : steps) {
+      step.trim();
+    }
+
+    Entity recipeEntity = new Entity("PrivateRecipe");
+    recipeEntity.setProperty("recipeName", recipeName);
+    recipeEntity.setProperty("tags", tags);
+    recipeEntity.setProperty("description", description);
+    recipeEntity.setProperty("ingredients", ingredients);
+    recipeEntity.setProperty("steps", steps);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(recipeEntity);
 
     response.sendRedirect("/pages/UserPage.html");
   }
-
 }
