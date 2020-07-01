65583660501306046edb452cad95db07fb1604ef
ali <ali@ibashir.com>
2020-07-01 18:15:29 +0000

    fix style in import files

--- src/main/java/com/google/sps/servlets/LoginServlet.java
+++ src/main/java/com/google/sps/servlets/LoginServlet.java
@@ -18,13 +18,14 @@ import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.google.sps.util.UserHelper;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.google.sps.util.UserHelper;
+
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
