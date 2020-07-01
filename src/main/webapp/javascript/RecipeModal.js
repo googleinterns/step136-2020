adb0c694521ba666b51347e6596c9247db1e25b8
Isabel <isamitre@gmail.com>
2020-07-01 14:51:50 +0000

    renamed javascript file

--- src/main/webapp/javascript/RecipeModal.js
+++
@@ -1,8 +0,0 @@
-
-function openModal() {
-  document.getElementById("recipe-modal").style.display = "block";
-}
-
-function closeModal() {
-  document.getElementById("recipe-modal").style.display = "none";
-}
