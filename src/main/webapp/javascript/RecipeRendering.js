500807101310a23a435d0b7177efe58444d53805
Mathew Suarez <masulo99@gmail.com>
2020-07-07 14:34:59 +0000

    Refactored to depricate the use of innerHTML = property. Current implementation uses JS dom manipulation, but remains flexible and quick by implementing a custom createElement method.

--- src/main/webapp/javascript/RecipeRendering.js
+++ src/main/webapp/javascript/RecipeRendering.js
@@ -10,41 +10,50 @@ createRecipeCard = (divId, recipeInfo) => {
     let docDiv = document.getElementById(divId);
 
     // Creates the recipeDiv container and sets the class name
-    let recipeDiv = document.createElement("div");
-    recipeDiv.setAttribute("class", "recipe-card");
+    let recipeDiv = createElement("div", "", {"class": "recipe-card"});
 
-    /**
-     * Creates the html content to include in the recipe card div.
-     * This approach was preferred to conserve space vs using 
-     * document.createElement and other methods 
-     */
-    let htmlString =
-        createElement(
-            removeHTML(recipeInfo["name"]),
-            "p", "class=\"recipe-card-name\"") +
-        createElement(
-            removeHTML(recipeInfo["description"]),
-            "p", "class=\"recipe-card-description\"");
+    // This is the list of all the elements which should be rendered on the recipe
+    // card. To change how ellements are displayed, change the order in which this
+    // list is initilaized
+    let allElementsToAdd = [
+        createElement(recipeInfo["name"], "p", {"class": "recipe-card-name"}),
+        createElement(recipeInfo["description"], "p", {"class": "recipe-card-description"}),
+    ];
 
-    recipeDiv.innerHTML = htmlString;
+    // Adds all the elements to the recipe card, then appends the recipe card
+    // to the div
+    allElementsToAdd.forEach(elem => recipeDiv.childNodes.push(elem));
     docDiv.appendChild(recipeDiv);
 }
 
 /**
  * Takes an object which can be represented as a string or a string, an html
- * tag, and the html tag options, to create the html text. Returns the resulting
- * text.
+ * tag, and the html tag options stored as an object. Returns the reference to the
+ * constructed html element. 
+ * Default values are set for object and tag options for instances where these aren't
+ * needed, such as when making a new container div.
  */
-createElement = (object, htmlTag, tagOptions) => {
-    return "<" + htmlTag + " " + tagOptions + ">" + object + "</" + htmlTag + ">";
-}
+createElement = (htmlTag, object = "", tagOptions = {}) => {
+    // Tries to create an HTML element with the given tag, otherwise logs the error
+    try {
+        let htmlElement = document.createElement(htmlTag.toLowerCase());
+    } catch (error) {
+        console.error("Cannot create an html element of type: " + htmlTag.toString() + "\n" + error);
+    }
 
-/**
- * Takes a string and replaces any HTML tags with the html entity name, preventing
- * HTML injection while reducing the work done.
- */
-removeHTML = (str) => {
-    let replaced = str.replace(/\</g, "&lt");
-    replaced = replaced.replace(/\>/g, "&gt");
-    return replaced;
+    let htmlText = document.createTextNode(object);
+    htmlElement.appendChild(htmlText);
+
+    // Goes through each option tries to add the attribute to the new HTML element.
+    // If it cannot add the attribute, it exits and logs the error
+    for (opt in tagOptions) {
+        try {
+            htmlElement.setAttribute(opt, tagOptions[opt]);
+        } catch (error) {
+            console.error ("There was an error trying to set element attibute: " +
+                opt.toString() + ":"  + tagOptions[opt].toString() + "\n", error);
+            return;
+        }
+    }
+    return htmlElement;
 }
