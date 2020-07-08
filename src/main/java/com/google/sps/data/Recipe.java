c1df404cd7f634b2d60760436e8607b0917d3999
Isabel <isamitre@gmail.com>
2020-07-08 18:31:53 +0000

    change recipeName to name

--- src/main/java/com/google/sps/data/Recipe.java
+++ src/main/java/com/google/sps/data/Recipe.java
@@ -11,14 +11,14 @@ public class Recipe {
   private String authorID;
   private String description;
   private String imageBlobKey;
-  private String recipeName;
+  private String name;
   private int popularity;
   private long id;
 
-  public Recipe(long id, String recipeName, String imageBlobKey, String description, ArrayList<String> tags, 
+  public Recipe(long id, String name, String imageBlobKey, String description, ArrayList<String> tags, 
       ArrayList<String> ingredients, ArrayList<String> steps, int popularity) {
     this.id = id;
-    this.recipeName = recipeName;
+    this.name = name;
     this.imageBlobKey = imageBlobKey;
     this.tags = tags;
     this.description = description;
