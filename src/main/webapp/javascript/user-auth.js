2b01d618c002b47f90355920983973e53eb380d5
ali <ali@ibashir.com>
2020-07-09 17:06:46 +0000

    full g-signin user box with dropdown

--- src/main/webapp/javascript/user-auth.js
+++ src/main/webapp/javascript/user-auth.js
@@ -1,65 +1,84 @@
+var auth2;
 
-// To be called before any request that requires user be logged in.
-// Action: redirects to user login page, then returns to redirectUrl.
-// Args: String redirectUrl defaults to the page function was called form.
-function confirmUserAndRedirect(redirectUrl) {
-  if (!redirectUrl) {
-    redirectUrl = window.location.href;
-  }
+function onStart() {
+  console.log("onstart");
+  gapi.load('auth2', initSigninV2);
+}
 
-  fetch(`/login?redirectUrl=${redirectUrl}`).then(response => response.json()).then(json => {
-      window.location.replace(json.loginLink);
+/**
+ * initializes google authentication API and sets event listeners.
+ */
+function initSigninV2() {
+  auth2 = gapi.auth2.init({
+    client_id : '1034390229233-u07o0iaas2oql8l4jhe7fevpfsbrtv7n.apps.googleusercontent.com'
+  });
+
+  auth2.currentUser.listen(function(newGoogleUser) {
+    insertUserInfo(newGoogleUser);
+  });
+
+  auth2.isSignedIn.listen(function(signedIn) {
+    if (signedIn) {
+      console.log('enable dropdown');
+      enableUserDropdown();
+    } else {
+      console.log('disable dropdown');
+      disableUserDropdown();
+    }
   });
 }
 
-function userLoggedIn() {
-  fetch("/user-status").then(response => response.json()).then(userInfo => {
-    return userInfo.isUserLoggedIn;
-  });
+/**
+ * returns a promise that resolves when user signs in,
+ * or rejects if user does not sign in.
+ * currently doesn't work because auth2.signin resolves before signin is complete
+ * for some reason.
+ */
+function confirmUser() {
+  return new Promise((resolve, reject) => {
+      if (!auth2.isSignedIn.get()) {
+          auth2.signIn().then(resolve()).catch(reject());
+      } else {
+          console.log("already done");
+          resolve();
+      }
+  })
 }
 
-// Action: requests logout .
-// Args: optional string "redirectUrl" determines location after logout.
-//       defaults to index.html.
-
-function logout(redirectUrl) {
-  if (!redirectUrl) {
-      redirectUrl = "/index.html";
-  }
-
-  fetch(`/logout?redirectUrl=${redirectUrl}`).then(response => response.json()).then(json => {
-    window.location.replace(json.logoutLink);
-  });
-}
-
-// sign out with google sign in API
+/**
+ * signs our current user from g-signin API.
+ */
 function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
-        // Disable user options dropdown menu on sign out.
-        const dropdown = document.querySelector(".dropdown");
-        dropdown.removeEventListener("mouseover", showDropdown);
-        dropdown.removeEventListener("mouseleave", hideDropdown);
-        dropdown.querySelector(".dropdown-content").style.display = "none";
-    });
+  auth2.signOut().then(function () {
+      console.log('User signed out.');
+  });
 }
 
-// In case of successfull sign in
-function onSignIn() {
-    console.log("login success");
-    // allows display of user options dropdown menu, only when signed in.
-    const dropdown = document.querySelector(".dropdown");
-    dropdown.addEventListener("mouseover", showDropdown = function() {
-        dropdown.querySelector(".dropdown-content").style.display = "block";
-    });
-    dropdown.addEventListener("mouseleave", hideDropdown = function() {
-        dropdown.querySelector(".dropdown-content").style.display = "none";
-    });
-    // adds current user name to dropdown
-    var auth2 = gapi.auth2.getAuthInstance();
-    const userPageAnchor = document.getElementById("user-page-anchor");
-    let text = auth2.currentUser.get().getBasicProfile().getGivenName() + "'s Page";
-    userPageAnchor.innerText = text;
+/**
+ * To be run in currentUser listener, activates whenever current user changes.
+ *
+ * @param {GoogleUser} current user to provide profile info.
+ */
+function insertUserInfo(googleUser) {
+  // insert user name into user-page anchor
+  let text = googleUser.getBasicProfile().getGivenName() + '\'s Page';
+  const userPageAnchor = document.getElementById('user-page-anchor');
+  userPageAnchor.innerText = text;
 }
 
+function enableUserDropdown() {
+  const dropdown = document.querySelector('.dropdown');
+  dropdown.addEventListener('mouseover', showDropdown = function() {
+    dropdown.querySelector('.dropdown-content').style.display = 'block';
+  });
+  dropdown.addEventListener('mouseleave', hideDropdown = function() {
+    dropdown.querySelector('.dropdown-content').style.display = 'none';
+  });
+}
+
+function disableUserDropdown() {
+  const dropdown = document.querySelector('.dropdown');
+  dropdown.removeEventListener('mouseover', showDropdown);
+  dropdown.removeEventListener('mouseleave', hideDropdown);
+  dropdown.querySelector('.dropdown-content').style.display = 'none';
+}
