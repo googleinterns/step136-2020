959949250c5bacf1dbf4d43e70ea75ebf842d220
ali <ali@ibashir.com>
2020-07-13 23:45:24 +0000

    Add List import

--- src/main/java/com/google/sps/data/User.java
+++ src/main/java/com/google/sps/data/User.java
@@ -14,6 +14,7 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
 
 public class User {
   // API variables:
