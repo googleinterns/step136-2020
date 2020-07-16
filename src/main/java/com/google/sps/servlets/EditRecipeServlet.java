0d4d123505ca92f84c6a35da55c77de02a3b2c3a
Isabel <isamitre@gmail.com>
2020-07-16 13:40:05 +0000

    edit recipe seems to actually work now

--- src/main/java/com/google/sps/servlets/EditRecipeServlet.java
+++ src/main/java/com/google/sps/servlets/EditRecipeServlet.java
@@ -8,6 +8,10 @@ import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.PreparedQuery.TooManyResultsException;
 import com.google.appengine.api.datastore.Query;
@@ -32,10 +36,9 @@ import javax.servlet.http.HttpServletResponse;
 public class EditRecipeServlet extends HttpServlet {
 
   @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException  {
     // get edits from form
     String idResponse = request.getParameter("recipeID");
-    System.out.println(idResponse);
     String name = request.getParameter("edit-name");
     String tagsResponse = request.getParameter("edit-tags");
     String description = request.getParameter("edit-description");
@@ -51,67 +54,68 @@ public class EditRecipeServlet extends HttpServlet {
 
     long id = Long.parseLong(idResponse);
 
-    // make a filter based on the id
-    Filter filter = new FilterPredicate("id", FilterOperator.EQUAL, id);
-    // set filters to the query
-    Query query = new Query("PrivateRecipe").setFilter(filter);
-
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    PreparedQuery results = datastore.prepare(query);
-    Entity recipeEntity = results.asSingleEntity();
+    
+    Key key = KeyFactory.createKey("PrivateRecipe", id);
+    try {
+      Entity recipeEntity = datastore.get(key);
 
-    // if the name input in the form is empty, the original value will be saved
-    if (name.equals("")) {
-      name = (String) recipeEntity.getProperty("name");
-    } else {
-      // formats the form response
-      name.trim();
+      // if the name input in the form is empty, the original value will be saved
+      if (name.equals("")) {
+        name = (String) recipeEntity.getProperty("name");
+      } else {
+        // formats the form response
+        name.trim();
+      }
+
+        // if the tags input in the form is empty, the original value will be saved
+      if (tagsResponse.equals("")) {
+        tags = (ArrayList<String>) recipeEntity.getProperty("tags");
+      } else {
+        // splits the String responses from the form by commas, trims each member of array, and makes the resulting arrays into Lists
+        tags = new ArrayList<String>(Arrays.asList(Arrays.stream(tagsResponse.split(",")).map(String::trim).toArray(String[]::new)));
+        // removes any empty or null members from the list
+        tags.removeAll(Arrays.asList("", null));
+      }
+
+      // if the description input in the form is empty, the original value will be saved
+      if (description.equals("")) {
+        description = (String) recipeEntity.getProperty("description");
+      } else {
+        // formats the form response
+        description.trim();
+      }
+
+      // if the ingredients input in the form is empty, the original value will be saved
+      if (ingredientsResponse.equals("")) {
+        ingredients = (ArrayList<String>) recipeEntity.getProperty("tags");
+      } else {
+        ingredients = new ArrayList<String>(Arrays.asList(Arrays.stream(ingredientsResponse.split("\n")).map(String::trim).toArray(String[]::new)));
+        ingredients.removeAll(Arrays.asList("", null, "\n", "\r\n", "\r"));
+      }
+
+      // if the ingredients input in the form is empty, the original value will be saved
+      if (stepsResponse.equals("")) {
+        steps = (ArrayList<String>) recipeEntity.getProperty("tags");
+      } else {
+        steps = new ArrayList<String>(Arrays.asList(Arrays.stream(stepsResponse.split("\n")).map(String::trim).toArray(String[]::new)));
+        steps.removeAll(Arrays.asList("", null, "\n", "\r\n", "\r"));
+      }
+
+      recipeEntity.setProperty("name", name);
+      recipeEntity.setProperty("tags", tags);
+      recipeEntity.setProperty("description", description);
+      recipeEntity.setProperty("ingredients", ingredients);
+      recipeEntity.setProperty("steps", steps);
+      recipeEntity.setProperty("imageBlobKey", (String) recipeEntity.getProperty("imageBlobKey"));
+
+      datastore.put(recipeEntity);
+
+    } catch (EntityNotFoundException e) {
+        System.out.println("entity not found exception");
     }
+    
 
-    // if the tags input in the form is empty, the original value will be saved
-    if (tagsResponse.equals("")) {
-      tags = (ArrayList<String>) recipeEntity.getProperty("tags");
-    } else {
-      // splits the String responses from the form by commas, trims each member of array, and makes the resulting arrays into Lists
-      tags = new ArrayList<String>(Arrays.asList(Arrays.stream(tagsResponse.split(",")).map(String::trim).toArray(String[]::new)));
-      // removes any empty or null members from the list
-      tags.removeAll(Arrays.asList("", null));
-    }
-
-    // if the description input in the form is empty, the original value will be saved
-    if (description.equals("")) {
-      description = (String) recipeEntity.getProperty("description");
-    } else {
-      // formats the form response
-      description.trim();
-    }
-
-    // if the ingredients input in the form is empty, the original value will be saved
-    if (ingredientsResponse.equals("")) {
-      ingredients = (ArrayList<String>) recipeEntity.getProperty("tags");
-    } else {
-      ingredients = new ArrayList<String>(Arrays.asList(Arrays.stream(ingredientsResponse.split("\n")).map(String::trim).toArray(String[]::new)));
-      ingredients.removeAll(Arrays.asList("", null, "\n", "\r\n", "\r"));
-    }
-
-    // if the ingredients input in the form is empty, the original value will be saved
-    if (stepsResponse.equals("")) {
-      steps = (ArrayList<String>) recipeEntity.getProperty("tags");
-    } else {
-      steps = new ArrayList<String>(Arrays.asList(Arrays.stream(stepsResponse.split("\n")).map(String::trim).toArray(String[]::new)));
-      steps.removeAll(Arrays.asList("", null, "\n", "\r\n", "\r"));
-    }
-
-    System.out.println("\nname value: " + name+"\n");
-
-    recipeEntity.setProperty("name", name);
-    recipeEntity.setProperty("tags", tags);
-    recipeEntity.setProperty("description", description);
-    recipeEntity.setProperty("ingredients", ingredients);
-    recipeEntity.setProperty("steps", steps);
-    recipeEntity.setProperty("imageBlobKey", (String) recipeEntity.getProperty("imageBlobKey"));
-
-    datastore.put(recipeEntity);
 
     response.sendRedirect("/pages/UserPage.html");
   }
