d5fbf54da12b53190fc39ece608c677c55586b69
ali <ali@ibashir.com>
2020-07-17 06:36:22 +0000

    User.java refactor -  now manages recipe keys rather than IDs -  includes Private/Public - currently implements test user

--- src/main/java/com/google/sps/data/User.java
+++ src/main/java/com/google/sps/data/User.java
@@ -5,20 +5,26 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.json.jackson2.JacksonFactory;
+
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
+
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+
 import java.io.IOException;
-import java.lang.reflect.Type;
+
 import java.lang.SecurityException;
+
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 import java.security.GeneralSecurityException;
 
 public class User {
@@ -48,23 +54,31 @@ public class User {
     "family_name" : "User",
   */
   private Payload payload;
-
   /**
+    User Entity Structure:
 
+    Key = User(userID) {
+      "name" : String,
+      "cookbook" : ArrayList<Key)>,    *May contain public or private recipe kind.
+      "planner" : ArrayList<Key)>,    *May contain public or private recipe kind.
+      "drafts" : ArrayList<Key>    *May contain private recipe kind.
+    }
+
+    * Recipe lists maintain the order they were added in.
     */
   private Entity entity;
-
+  private DatastoreService datastore;
   /**
    * Verifies user and creates a User instance for accessing and adding user data.
-   * throws SecurityException on failure, import from java.lang.SecurityException.
+   * Throws SecurityException on failure, import from java.lang.SecurityException.
    * @Param idTokenString is the Google-user ID Token provided buy user-auth.js/getIdToken.
    * Token should be passed as URL Fetch argument from front end.
    */
   public User(String idTokenString) throws SecurityException {
     // GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier
-    // .Builder(UrlFetchTransport.getDefaultInstance(), new JacksonFactory())
-    // .setAudience(Collections.singletonList(CLIENT_ID))
-    // .build();
+    //     .Builder(UrlFetchTransport.getDefaultInstance(), new JacksonFactory())
+    //     .setAudience(Collections.singletonList(CLIENT_ID))
+    //     .build();
 
     // GoogleIdToken idToken;
     // try {
@@ -74,17 +88,16 @@ public class User {
     // }
 
     // payload = idToken.getPayload();
-
+    datastore = DatastoreServiceFactory.getDatastoreService();
     Key userKey = KeyFactory.createKey("User", getId());
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
     try {
+      // Check if User already has a profile
       entity = datastore.get(userKey);
     } catch(EntityNotFoundException e) {
+      // Create initial User instance, default name from idToken payload.
       entity = new Entity(userKey);
-      entity.setProperty("displayName", "testman jones");
-      entity.setProperty("cookbook", null);
-      entity.setProperty("userRecipes", null);
-      entity.setProperty("planner", null);
+      entity.setProperty("name", "TestPersson Userson");
     }
   }
 
@@ -93,7 +106,7 @@ public class User {
    */
   public String getId() {
     //return payload.getSubject();
-    return "0000000000"
+    return "user-test-ID-string";
   }
 
   public String getName() {
@@ -105,23 +118,43 @@ public class User {
     uploadEntity();
   }
 
-  public List<Key> getCookbook();
+  public List<Key> getCookbookList() {
+    return getRecipeList(COOKBOOK);
+  }
 
-  public List<Key> getUserRecipes();
+  public List<Key> getPlannerList() {
+    return getRecipeList(PLANNER);
+  }
 
-  public List<Key> getPlanner();
+  public List<Key> getDraftList() {
+    return getRecipeList(DRAFTS);
+  }
 
-  public void addCookbookKey(Key key);
+  public void addCookbookKey(Key key) {
+    addKey(key, COOKBOOK);
+  }
 
-  public void addPlannerKey(Key key);
+  public void addPlannerKey(Key key) {
+    addKey(key, PLANNER);
+  }
 
-  public void addDraftKey(Key key);
+  public void addDraftKey(Key key) {
+    addKey(key, DRAFTS);
+  }
 
-  public void removeCookbookKey(Key key);
+  public void removeCookbookKey(Key key) {
+    removeKey(key, COOKBOOK);
+  }
 
-  public void removePlannerKey(Key key);
+  public void removePlannerKey(Key key) {
+    removeKey(key, PLANNER);
+  }
 
-  public void removeDraftKey(Key key);
+  public void removeDraftKey(Key key) {
+    removeKey(key, DRAFTS);
+  }
+
+  // PUBLIC METHODS END HERE
 
   /**
    * Adds the current version of User entity to datastore.
@@ -132,5 +165,36 @@ public class User {
   }
 
   /**
+   * Helper method for retrieving ArrayList<Key> of given recipe list.
+   */
+  @SuppressWarnings("unchecked") // Compiler cannot verify generic property type.
+  private ArrayList<Key> getRecipeList(String listName) {
+    ArrayList<Key> arrayListValue = (ArrayList<Key>) entity.getProperty(listName);
+    if (arrayListValue == null) {
+      return new ArrayList<Key>();
+    }
+    return arrayListValue;
+  }
 
+  /**
+   * Helper method for adding a key to a given recipe list.
+   */
+  private void addKey(Key key, String listName) {
+    ArrayList<Key> keys = getRecipeList(listName);
+    if (!keys.contains(key)) {
+      keys.add(key);
+      entity.setProperty(listName, keys);
+      uploadEntity();
+    }
+  }
+
+  /**
+   * Helper methods for deleting a key from a given recipe list.
+   */
+  private void removeKey(Key key, String listName) {
+    ArrayList<Key> keys = getRecipeList(listName);
+    if (keys.remove(key)) {
+      uploadEntity();
+    }
+  }
 }
