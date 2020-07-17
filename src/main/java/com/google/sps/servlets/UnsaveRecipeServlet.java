421a4e1396f6a8e2d2242bb08b545c72dded9b7c
ali <ali@ibashir.com>
2020-07-17 21:33:02 +0000

    UnsaveRecipeServlet removes recipe key from desired user list

---
+++ src/main/java/com/google/sps/servlets/UnsaveRecipeServlet.java
@@ -0,0 +1,65 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.gson.Gson;
+import com.google.sps.data.User;
+import java.io.IOException;
+import java.lang.SecurityException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Remove recipe from user's list depending on the following parameters:
+ * "listName" = "cookbook", "planner". The list to delete from.
+ * "isPublic" = Boolean
+ * "recipeId" = Long
+ *  userToken is the user's Google id_token provided by user-auth.js/getIdToken().
+ * "userToken" = String
+ */
+@WebServlet("/unsave-recipe")
+public class UnsaveRecipeServlet extends HttpServlet {
+
+  private static final String COOKBOOK = "cookbook";
+  private static final String PLANNER = "planner";
+  private static final String PUBLIC_KIND = "PublicRecipe";
+  private static final String PRIVATE_KIND = "PrivateRecipe";
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String userToken = (String) request.getParameter("idToken");
+    String listName = (String) request.getParameter("listName");
+    Boolean isPublic = (Boolean) request.getParameter("isPublic");
+    long recipeId = Long.parseLong(request.getParameter("recipeId"));
+
+    User currentUser = new User(userToken);
+
+    String recipeKind = (isPublic) ? recipeKind = PUBLIC_KIND : recipeKind = PRIVATE_KIND;
+    
+    Key recipeKey = KeyFactory.createKey(recipeKind, recipeId);
+    
+    if (listName.equals(COOKBOOK)) {
+      currentUser.removeCookbookKey(recipeKey);
+    } else if (listName.equals(PLANNER)) {
+      currentUser.removePlannerKey(recipeKey);
+    } else {
+      System.out.println("Invalid listName " + destination + ". No action.");
+    }
+  }
+}
