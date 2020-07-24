f667dc478731b5f4e26b029a32113f7fd128cfa6
Mathew Suarez <masulo99@gmail.com>
2020-07-24 16:11:11 +0000

    Updated functions to handle the loading of the recipe template page, and added function to redirect from recipe card to recipe template page.

--- src/main/webapp/javascript/recipe-rendering.js
+++ src/main/webapp/javascript/recipe-rendering.js
@@ -18,9 +18,10 @@ createRecipeCard = (divID, recipeInfo) => {
     createElement("button", "Cookbook ", {"class": "card-button bottom more-right cookbook-btn"}),
   ];
   elementsToAddToImageDiv.forEach(elem => imageDiv.appendChild(elem));
-
   let elementsToAddToTextDiv = [
-    createElement("p", recipeInfo["name"], {"class": "recipe-card-name"}),
+    createElement("button", recipeInfo["name"], 
+      {"class": "recipe-card-name", 
+      "onClick": "redirectRecipePage(" + recipeInfo["id"].toString() + ")"}),
     createElement("p", recipeInfo["description"], {"class": "recipe-card-description"}),
   ];
   elementsToAddToTextDiv.forEach(elem => textDiv.appendChild(elem));
@@ -50,11 +51,77 @@ createRecipeCard = (divID, recipeInfo) => {
 }
 
 /**
- * Takes the id for a recipe in Datastore, and using the recipe page template
- * completes the page for the user. This function is added to each recipe card
- * name, and should be called onClick().
+ * Takes the id for a recipe in Datastore, and redirects to the recipe page template
+ * with the recipe ID as a search param.
  */
-async function renderRecipePage(id) {
+function redirectRecipePage(id) {
+  window.location.assign("/pages/RecipePageTemplate.jsp?id=" + id.toString());
+}
+
+/**
+ * Function for loading the recipe template page based on the id in the URL
+ * search params.
+ */
+async function fillRecipeTemplate() { 
+  const recipeId = (new URL(document.location)).searchParams.get("id");
+
+  // Gets the servlet data for the recipe
+  // TODO: put this code in an if statement to handle recipe not found scenarios
+  let response = await fetch("/retrieve-id?recipeId=" + recipeId.toString());
+  let responseText = await response.text();
+  console.log(responseText);
+  let recipeInfo = JSON.parse(responseText);
+
+  let detailsSection = document.getElementById("details");
+  let imageSection = document.getElementById("image-container");
+  let descriptionSection = document.getElementById("description");
+  let ingredientSection = document.getElementById("ingredients");
+  let stepsSection = document.getElementById("steps");
+
+  addRecipeInfo(recipeInfo, detailsSection);
+  imageSection.appendChild(createImage(recipeInfo["name"], recipeInfo["imageBlobKey"]));
+  descriptionSection.appendChild(createElement(
+      "p", recipeInfo["description"], {"id": "description-text"}));
+  addAsList(recipeInfo["ingredients"], ingredientSection, "Ingredients");
+  addAsList(recipeInfo["steps"], stepsSection, "Steps");
+}
+
+/**
+ * Creates the HTML for the information about the recipe and adds it to the div.
+ * Current implementation adds the recipe name and the author.
+ * Takes the recipe object and div (not div name) being used.
+ */
+addRecipeInfo = (recipeObj, infoDiv) => {
+  let elementsForDiv = [
+    createElement("h1", recipeObj["name"], {"id": "recipe-name"})/*,
+    createElement("h3", recipeObj["author"], {"id": "recipe-author"})*/
+    // Uncomment once the backend can handle sending author info too
+  ];
+  
+  // Adds all the recipe info to the passed div
+  elementsForDiv.forEach(elem => infoDiv.appendChild(elem));
+}
+
+/**
+ * Creates the HTML for some part of the recipe which needs to shown as a list
+ * (ingredients, steps, etc.) and adds it to the given div.
+ * Takes a list and div (not div name) being used.
+ */
+addAsList = (listObj, listDiv, listName) => {
+  // Constrcuts the base of how all item created in the function will be 
+  // named for CSS
+  let cssRef = listName.toLowerCase() + "-list";
+
+  // Creates the title for the list, and the list element
+  let listForDiv = createElement("ol", "", {"id": cssRef})
+  let listTitle = createElement("h2", listName, {"id": cssRef + "-title"});
+  
+  // Goes through all the objects in listObj and adds them as items to listForDiv
+  listObj.forEach(item => listForDiv.appendChild(
+    createElement("li", item, {"class": cssRef + "-item"})));
+  
+  listDiv.appendChild(listTitle);
+  listDiv.appendChild(listForDiv);
 }
 
 /**
@@ -68,16 +135,16 @@ createElement = (htmlTag, object = "", tagOptions = {}) => {
     // Create an HTML element with the given tag
     let htmlElement = document.createElement(htmlTag.toLowerCase());
 
-    // If the object parameter is not undefined or an empty string, it 
+    // If the object parameter is not null or an empty string, it 
     // creates a textnode and adds it to the HTML element
-    if (object !== undefined && object !== "") {
+    if (object !== null && object !== "") {
       let htmlText = document.createTextNode(object);
       htmlElement.appendChild(htmlText);
     }
 
-    // If the tagOptions object is not undefined or empty, it goes through each 
+    // If the tagOptions object is not null or empty, it goes through each 
     // option and adds the attribute to the new HTML element.
-    if (tagOptions !== undefined && Object.keys(tagOptions).length > 0) {
+    if (tagOptions !== null && Object.keys(tagOptions).length > 0) {
       for (opt in tagOptions) {   
           htmlElement.setAttribute(opt, tagOptions[opt]); 
       }
