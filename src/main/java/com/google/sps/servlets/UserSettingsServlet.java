10d5adb3208a8b2764681315c7026d77e30dbed9
ali <ali@ibashir.com>
2020-07-29 00:50:13 +0000

    fixed missing import

--- src/main/java/com/google/sps/servlets/UserSettingsServlet.java
+++ src/main/java/com/google/sps/servlets/UserSettingsServlet.java
@@ -4,6 +4,7 @@ import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
