58204a0f818840b321f5aed5843b6e5ca319f60f
Isabel <isamitre@gmail.com>
2020-07-30 13:05:45 +0000

    working on it

--- src/main/java/com/google/sps/servlets/ListShoppingListServlet.java
+++ src/main/java/com/google/sps/servlets/ListShoppingListServlet.java
@@ -29,40 +29,15 @@ public class ListShoppingListServlet extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String idToken = request.getParameter("idToken");
     User user = new User(idToken);
-    String authorID = user.getId();
-
-    List<Ingredients> ingredients = {new Ingredient()};
-    if (type.equals("cookbook")) {
-      keys = user.getCookbookList();
-    } else if (type.equals("planner")) {
-      keys = user.getPlannerList();
-    } else {
-      keys = Collections.emptyList();
-      System.out.println("ListTypeRecipesServlet: invalid type");
-    }
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     
-    List<Recipe> recipes = new ArrayList<>();
-    for (Key key : keys) {
-      try {
-        Entity entity = datastore.get(key);
-        long id = key.getId();
-        String name = (String) entity.getProperty("name");
-        String description = (String) entity.getProperty("description");
-        String blobkey = (String) entity.getProperty("imageBlobKey");
-
-        Recipe recipe = new Recipe(id, name, blobkey, description);
-        recipes.add(recipe);
-      } catch (EntityNotFoundException e ) {
-        System.out.println("ListTypeRecipesServlet: Entity not found with key from user key list. This should never happen.");
-      }
-    }
+    List<Recipe> ingredients = new ArrayList<>();
  
     Gson gson = new Gson();
 
     response.setContentType("application/json;");
-    response.getWriter().println(gson.toJson(recipes));
+    response.getWriter().println(gson.toJson(ingredients));
   }
 }
  
