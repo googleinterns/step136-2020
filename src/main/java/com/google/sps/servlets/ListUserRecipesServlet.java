181e85765e1b43ea8c74068eb2474dd9dbfb9077
Isabel <isamitre@gmail.com>
2020-08-04 20:20:38 +0000

    remove outdated code

--- src/main/java/com/google/sps/servlets/ListUserRecipesServlet.java
+++ src/main/java/com/google/sps/servlets/ListUserRecipesServlet.java
@@ -33,8 +33,7 @@ public class ListUserRecipesServlet extends HttpServlet {
    
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String idToken = null;
-    idToken = request.getParameter("idToken");
+    String idToken = request.getParameter("idToken");
     User user;
     // if user is not logged in, redirects back to Home
     try {
